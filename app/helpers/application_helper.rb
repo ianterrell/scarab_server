377bfcbcf43da5f272f86a05101ce7c26f239625
Ian Terrell <ian.terrell@gmail.com>
2009-07-31 23:02:09 -0400

    iphone display work

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -25,7 +25,7 @@ module ApplicationHelper
   
   def iphone_display(copy)
     content_tag :div, :class => "iphone-display" do
-      (copy || "").gsub("\n", content_tag(:br))
+      (copy || "").gsub("\n", content_tag(:br)).gsub(" ", "&nbsp;")
     end
   end
   
