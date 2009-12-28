8caa27a5babdc589065d2e3d031569ad0216065e
Ian Terrell <ian.terrell@gmail.com>
2009-12-28 16:18:21 -0500

    added previous and next issue links

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -5,5 +5,11 @@ class IssuesController < ApplicationController
   def show
     @issue = Issue.find_by_number params[:number]
     redirect_to issue_path :number => @current_issue.number and return if @issue.nil? || !@issue.published?
+    if @issue
+      @previous_issue = Issue.find_by_number @issue.number - 1
+      @previous_issue = nil unless @previous_issue && @previous_issue.published?
+      @next_issue = Issue.find_by_number @issue.number + 1
+      @next_issue = nil unless @next_issue && @next_issue.published?
+    end
   end
 end
