5dba4d0cc98eb7dae11d09e01cee6335d572881c
Ian Terrell <ian.terrell@gmail.com>
2010-01-17 18:56:29 -0500

    fixed bug with given device token string

--- app/controllers/api/v1/devices_controller.rb
+++ app/controllers/api/v1/devices_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::DevicesController < Api::V1::ApiController
   # Sort of fuzzy naming here to fit ObjectiveResource & REST.  
   # Really more like create_or_update
   def create
-    @device = APN::Device.find_by_token(params[:device][:token]) || APN::Device.new(params[:device])
+    @device = APN::Device.find_by_token(params[:device][:token].scan(/\<(.+)\>/).first) || APN::Device.new(params[:device])
     @device.last_registered_at = Time.now
     if @device.save
       render :xml => @device.to_xml(:only => [:id])
