d0562d32f70aaa72a2d007ef3d5fee7cab65b2e8
Ian Terrell <ian.terrell@gmail.com>
2010-01-18 10:43:04 -0500

    only send feedback emails on save, not save AND create

--- app/models/feedback_observer.rb
+++ app/models/feedback_observer.rb
@@ -1,9 +1,4 @@
 class FeedbackObserver < ActiveRecord::Observer
-  def after_create(feedback)
-    FeedbackMailer.deliver_feedback_received feedback
-  end
-
-  # Really just for initial feedback that's already been received
   def after_save(feedback)
     FeedbackMailer.deliver_feedback_received feedback
   end
