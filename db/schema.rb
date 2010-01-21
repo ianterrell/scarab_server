b990b01a78ab5896b4b4357f8aad22d0ba98b87d
Ian Terrell <ian.terrell@gmail.com>
2010-01-21 13:05:56 -0500

    migrated

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100118003853) do
+ActiveRecord::Schema.define(:version => 20100118154644) do
 
   create_table "apn_devices", :force => true do |t|
     t.string   "token",              :default => "",   :null => false
@@ -77,7 +77,7 @@ ActiveRecord::Schema.define(:version => 20100118003853) do
   end
 
   create_table "feedbacks", :force => true do |t|
-    t.string   "text"
+    t.text     "text"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -177,6 +177,12 @@ ActiveRecord::Schema.define(:version => 20100118003853) do
     t.string   "version_external_identifier"
     t.boolean  "apple_validated"
     t.datetime "purchased_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "twitter_giveaway_entries", :force => true do |t|
+    t.string   "username"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
