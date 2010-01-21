49e948dd91037090acd95b3cac4b79be2ba66697
Ian Terrell <ian.terrell@gmail.com>
2010-01-21 13:05:29 -0500

    added grackle gem for twitter integration

--- config/environment.rb
+++ config/environment.rb
@@ -23,6 +23,7 @@ Rails::Initializer.run do |config|
   config.gem 'yamler'
   config.gem 'configatron'
   config.gem 'apn_on_rails'
+  config.gem 'grackle'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
